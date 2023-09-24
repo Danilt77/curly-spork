c2478a758593bb5a23c06737f7c9f15606684b84
Danilt77 <100009452+Danilt77@users.noreply.github.com>
2023-09-24 14:45:41 +0500

    Add files via upload

--- Mountains/script.js
+++ Mountains/script.js
@@ -12,3 +12,10 @@ document.getElementsByClassName('nav__close')[0].onclick = function(event) {
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// слушаем событие resize
+window.addEventListener('resize', () => {
+    // получаем текущее значение высоты
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
