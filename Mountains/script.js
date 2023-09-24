dd099b27fb0d412f94142a4a8def91bf4dd20974
Danilt77 <100009452+Danilt77@users.noreply.github.com>
2023-09-24 14:36:55 +0500

    Fix

--- Mountains/script.js
+++ Mountains/script.js
@@ -6,3 +6,9 @@ document.getElementsByClassName('nav__open-btn')[0].onclick = function(event) {
 document.getElementsByClassName('nav__close')[0].onclick = function(event) {
     document.getElementsByClassName('nav__menu-opened')[0].style.left = '100%';
 }
+
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
