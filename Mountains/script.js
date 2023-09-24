e49c181897f8396061b6bacb39dc9f47ccdcc825
Danilt77 <100009452+Danilt77@users.noreply.github.com>
2023-09-24 14:14:42 +0500

    Upload files

---
+++ Mountains/script.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName('nav__open-btn')[0].onclick = function(event) {
+    document.getElementsByClassName('nav__menu-opened')[0].style.left = 0;
+    document.getElementsByClassName('nav__close')[0].style.display = 'block';
+}
+
+document.getElementsByClassName('nav__close')[0].onclick = function(event) {
+    document.getElementsByClassName('nav__menu-opened')[0].style.left = '100%';
+}
